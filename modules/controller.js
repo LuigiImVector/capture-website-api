c545066d1eff6c1ef1e14d25ee729b3391385141
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2022-12-28 20:33:42 +0100

    add try/catch to controller.js + fix

--- modules/controller.js
+++ modules/controller.js
@@ -5,20 +5,31 @@ import fs from 'fs';
 export const getImage = async (url = 'https://github.com/LuigiImVector/scrnsht/', res) => {
     const imagePath = './tmp/screenshot.png';
 
-    if (fs.existsSync(imagePath)) {
-        fs.unlink(imagePath, (err => {
-            if (err) console.log(err);
-            else {
-                console.log(`\nDeleted file: ${imagePath}`);
-            }
-        }));
+    try {
+        if (fs.existsSync(imagePath)) {
+            fs.unlink(imagePath, (err => {
+                if (err) console.log(err);
+                else {
+                    console.log(`\nDeleted file: ${imagePath}`);
+                }
+            }));
+        }
+
+        await captureWebsite.file(url, imagePath, {
+            width: 1920,
+            height: 1080
+        });
+
+        res.writeHead(200, { 
+            'content-type': mime.getType(imagePath),
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+            'Access-Control-Max-Age': 2592000
+        });
+        fs.createReadStream(imagePath).pipe(res);
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.write(String(error));
+        res.end();
     }
-
-    await captureWebsite.file(url, imagePath, {
-        width: 1920,
-        height: 1080
-    });
-
-    res.writeHead(200, { 'content-type': mime.getType(imagePath) });
-    fs.createReadStream(imagePath).pipe(res);
 };
